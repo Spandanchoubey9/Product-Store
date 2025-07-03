98ce27515ba32c857c307a05d62d44812b3ddf34
Spandanchoubey9 <spandanchoubey@gmail.com>
2025-07-03 22:45:09 +0530

    Added mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+
 const app = express();
 
-app.listen(5000, () => {
-
-console.log("Server started at http://localhost:5000 ");
+app.get("/products", (req,res) => {
+    res.send("Server is Ready ");
 });
 
+app.listen(3000, () => {
+console.log("Server started at http://localhost:3000");
+});
 
